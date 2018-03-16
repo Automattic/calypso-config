6365e507a6f5d4107d377502a7931f1e99f50c7d
Mohammad Jangda <batmoo@gmail.com>
2018-03-16 13:44:55 -0400

    Add client.js file at root to simplify `require`
    Instead of needing to do `@automattic/calypso-config/src/client`, can
    just do `@automattic/calypso-config/config`

---
+++ client.js
@@ -0,0 +1,3 @@
+// Utility file to simplify loading the client module.
+// Instead of require-ing `calypso-config/src/config`, apps can leave out the `src` bit.
+module.exports = require( './src/client' );
