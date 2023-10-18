6593db6fef73afdae7f3bb4a3f8930b1a872b865
Max Schmeling <max.schmeling@automattic.com>
2023-10-18 12:07:01 -0500

    Adds convenience export for universal-client

--- client.js
+++ client.js
@@ -1,3 +1,3 @@
 // Utility file to simplify loading the client module.
-// Instead of require-ing `calypso-config/src/config`, apps can leave out the `src` bit.
+// Instead of require-ing `calypso-config/src/client`, apps can leave out the `src` bit.
 module.exports = require( './src/client' );
